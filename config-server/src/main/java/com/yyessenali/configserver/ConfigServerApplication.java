9b2db11b9dee5f51696b38490dc1560a859d99f1
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2024-10-02 19:12:27 +0500

    init config, discovery, gateway projects

---
+++ config-server/src/main/java/com/yyessenali/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.yyessenali.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
